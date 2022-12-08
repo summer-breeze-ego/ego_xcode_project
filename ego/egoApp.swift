d79046833b1a6512b16b0f184eeeef4a3f520cc6
Augustin Coman <augustcmn@gmail.com>
2022-12-08 22:15:59 +0100

    Initial Commit

---
+++ ego/egoApp.swift
@@ -0,0 +1,17 @@
+//
+//  egoApp.swift
+//  ego
+//
+//  Created by Augustin Coman on 08/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct egoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
